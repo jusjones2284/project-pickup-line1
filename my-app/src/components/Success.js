40fab55ade820be278efe7453d04c06ce8e23716
Justin Jones <jus.jones@yahoo.com>
2022-12-09 15:17:51 -0500

    added new component

---
+++ my-app/src/components/Success.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+
+const Success =({ successStories })=>{
+ const allSuccessStories =  successStories.map((success)=>{
+    return(
+        
+        <ul><li>{success.content}</li></ul>
+    )
+   })
+   
+    return(
+        <>
+        <h1>Success Stories</h1>
+        {allSuccessStories}
+        </>
+      
+    )
+}
+
+export default Success
+
